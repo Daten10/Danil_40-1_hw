0896d4cdeeb97c5989d34be2cacc170b1bccdfc8
Daten10 <tendanil563@gmail.com>
2024-03-13 18:10:04 +0600

    v1

--- main1.py
+++ main1.py
@@ -7,6 +7,7 @@ def start_game():
     print('MY balance: ', config('MY_MONEY'), '$')
     list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     money = config('MY_MONEY', cast=int)
+    last_count = config('MY_MONEY', cast=int)
     while(exit!=0):
         guess = int(input('Выберите число от 1 до 10: '))
         if guess > 10 or guess < 1:
@@ -29,3 +30,9 @@ def start_game():
             print('Мой баланс: ', money,'$')
 
         exit = int(input('введите 0 чтобы выйти: '))
+        if exit == 0:
+            if money > config('MY_MONEY', cast=int):
+                print(f'Итог, вы в плюсе на {money - last_count}$')
+            elif money < config('MY_MONEY', cast=int):
+                print(f'Итог, вы в минусе на {last_count - money}$')
+
